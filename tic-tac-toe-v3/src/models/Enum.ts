4d6cd8dbe468a1130a721c1c7716f3a62729dfae
Henrique Tomaz <henriquetomaz021@gmail.com>
2026-04-16 20:45:25 -0300

    chore: implement object-oriented programming in the tic-tac-toe game

---
+++ tic-tac-toe-v3/src/models/Enum.ts
@@ -0,0 +1,11 @@
+export enum Peca {
+  Xis = 'X',
+  Circulo = 'O'
+}
+
+export enum SituacaoPartida {
+  VitoriaJogador1 = 'VitoriaJogador1',
+  VitoriaJogador2 = 'VitoriaJogador2',
+  Empate = 'Empate',
+  EmAndamento = 'EmAndamento'
+}
